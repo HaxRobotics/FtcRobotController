64590073489fd5588e1e315a41dc0ebdd11a62f1
Alexa Becker <alexa_becker@loomis.org>
2021-12-10 17:33:05 -0500

    mostly working auto, some bugs with timing/arm movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -18,6 +18,7 @@ public class Auto extends LinearOpMode {
     ElapsedTime time = new ElapsedTime();
     int width = 352;
     int height = 288;
+    int location = 0;
     // store as variable here so we can access the location
     ShippingElementDetector detector = new ShippingElementDetector(width);
 
@@ -55,40 +56,46 @@ public class Auto extends LinearOpMode {
         });
 
         webcam.setPipeline(detector);
-        ShippingElementDetector.ElementLocation location = detector.getLocation();
 
         waitForStart();
 
         time.reset();
-        while(time.seconds() < 2){
+        while(time.seconds() < 2 && opModeIsActive()){
             telemetry.addData("position", detector.getLocation());
-            location = detector.getLocation();
+            if(detector.location == ShippingElementDetector.ElementLocation.LEFT){
+                location = 1;
+            } else if (detector.location == ShippingElementDetector.ElementLocation.RIGHT) {
+                location = 3;
+            } else {
+                location = 2;
+            }
             telemetry.addData("position variable", location);
             telemetry.update();
         }
+
         // drives forward to turning position
         time.reset();
-        while (time.seconds() < .3) {
+        while (time.seconds() < .3 && opModeIsActive()) {
             drive.drive(.6, 0, 0);
         }
         // turns 180 degrees so robot faces towards wall
         time.reset();
-        while (time.seconds() < 1.55) {
+        while (time.seconds() < 1.55 && opModeIsActive()) {
             drive.drive(0, 0, .4);
         }
         // strafes so robot is pointing at carousel; just off the wall
         time.reset();
-        while (time.seconds() < .7) {
+        while (time.seconds() < .7 && opModeIsActive()) {
             drive.drive(0, -.6, 0);
         }
         // drives too carousel with lower speed
         time.reset();
-        while (time.seconds() < 1.5) {
+        while (time.seconds() < 1.5 && opModeIsActive()) {
             drive.drive(.2, 0, 0);
         }
         // spins wheel to spin one duck off carousel
         time.reset();
-        while (time.seconds() < 2.5) {
+        while (time.seconds() < 2.5 && opModeIsActive()) {
             carousel.start();
             drive.drive(0, 0, 0);
         }
@@ -96,34 +103,80 @@ public class Auto extends LinearOpMode {
 
         // backs up from carousel
         time.reset();
-        while (time.seconds() < .3) {
+        while (time.seconds() < .25 && opModeIsActive()) {
             drive.drive(-.6,0,0);
         }
 
         // turns 180 degrees to face opposite wall/storage hub
         time.reset();
-        while (time.seconds() < 1.55) {
+        while (time.seconds() < 1.45 && opModeIsActive()) {
             drive.drive(0,0,.4);
         }
 
         // strafes to line up with storage hub
         time.reset();
-        while (time.seconds() < 1.5) {
+        while (time.seconds() < 1.5 && opModeIsActive()) {
             drive.drive(0,-.6,0);
+        }
+
+        time.reset();
+        while (time.seconds() < .5 && opModeIsActive()) {
+            drive.drive(0,0,0);
         }
 
         // drives up to storage hub
         time.reset();
-        while (time.seconds() < .2) {
+        while (time.seconds() < .1 && opModeIsActive()) {
             drive.drive(.6,0,0);
         }
-        // lifts arm to correct level
 
         time.reset();
-        while (time.seconds() < 4) {
-            if(location == ShippingElementDetector.ElementLocation.LEFT) {
+        while (time.seconds() < 2 && opModeIsActive()) {
+            drive.drive(0,0,0);
+        }
+
+        // drives up to storage hub
+        /*time.reset();
+        while (time.seconds() < .2 && opModeIsActive()) {
+            drive.drive(.6,0,0);
+        }
+
+        time.reset();
+        while (time.seconds() < .5 && opModeIsActive()) {
+            drive.drive(0,0,0);
+        } */
+
+        // lifts arm to correct level
+        time.reset();
+        while (time.seconds() < 1 && opModeIsActive()) {
+            if(location == 1) {
                 arm.goTo(1);
+            } else if (location == 3) {
+                arm.goTo(3);
+            } else {
+                arm.goTo(2);
             }
+        }
+
+        time.reset();
+        while (time.seconds() < 1 && opModeIsActive()) {
+            intake.out(1);
+        }
+        intake.stop();
+
+        time.reset();
+        while (time.seconds() < 1 && opModeIsActive()) {
+            drive.drive(0,0,0);
+        }
+
+        time.reset();
+        while(time.seconds() < .2 && opModeIsActive()) {
+            drive.drive(-.6,0,0);
+        }
+
+        time.reset();
+        while (time.seconds() < 1 && opModeIsActive()) {
+            drive.drive(0,.6,0);
         }
     }
 }
